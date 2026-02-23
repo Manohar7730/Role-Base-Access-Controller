370f6d656b24da5bc6e919605f5b4249448dff63
Manohar <manohar20century@gmail.com>
2026-02-23 16:02:31 +0530

    feat(frontend) : add protected route based one authentication

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "../pages/Register";
 import Login from "../pages/Login";
 import Dashboard from "../pages/Dashboard";
+import ProtectedRoute from "./ProtectedRoute";
 
 export default function AppRouter() {
   return (
@@ -10,7 +11,9 @@ export default function AppRouter() {
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
